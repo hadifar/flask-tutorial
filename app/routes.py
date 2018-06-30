ac26b61cc56ba1ae71359123281e12a20f300609
Amir Hadifar <hadifar.amir@gmail.com>
2018-06-30 08:46:00 +0430

    login added

--- app/routes.py
+++ app/routes.py
@@ -14,7 +14,9 @@
 # limitations under the License.
 # ==============================================================================
 from flask import render_template
+
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -37,3 +39,9 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
